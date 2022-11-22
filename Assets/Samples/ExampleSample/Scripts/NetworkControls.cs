7ac814399f5f7b39fe3429768ef9e61863acdfad
Nick Maltbie <nick.dmalt@gmail.com>
2022-11-21 17:00:28 -0800

    Started setting up webgl build

---
+++ Assets/Samples/ExampleSample/Scripts/NetworkControls.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static UnityEngine.InputSystem.HID.HID;
+
+namespace nickmaltbie.NetworkStateMachineUnity.ExampleAnim
+{
+    public class NetworkControls : MonoBehaviour
+    {
+        public Button hostButton;
+        public Button clientButton;
+        public Button serverButton;
+        public TMP_InputField serverAddress;
+        public TMP_InputField serverPort;
+        public TMP_Text debugMessage;
+
+        public void Awake()
+        {
+            GameObject newtorkManager
+        }
+    }
+}
