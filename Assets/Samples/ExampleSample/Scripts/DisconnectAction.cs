ded34c8bbfa71ad1f32d8abe1f6c68718abca71e
Nick Maltbie <nick.dmalt@gmail.com>
2022-11-22 01:45:07 -0800

    Fixed some formatting errors

--- Assets/Samples/ExampleSample/Scripts/DisconnectAction.cs
+++ Assets/Samples/ExampleSample/Scripts/DisconnectAction.cs
@@ -16,7 +16,6 @@
 // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using Netcode.Transports.WebSocket;
 using nickmaltbie.ScreenManager;
 using Unity.Netcode;
 using UnityEngine;
